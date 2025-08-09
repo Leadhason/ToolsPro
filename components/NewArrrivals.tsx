58d2667f167acc224254797da86a81abc6028264
Leaderson <leadhason111@gmail.com>
2025-08-09 02:20:00 +0000

    updated product cards

--- components/NewArrrivals.tsx
+++ components/NewArrrivals.tsx
@@ -17,7 +17,7 @@ export default function NewArrivalsSection({ products }: NewArrivalsSectionProps
           </Link>
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3 sm:gap-4 lg:gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-5 sm:gap-4 lg:gap-8">
           {products.slice(0, 6).map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
