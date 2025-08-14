baefbefbcd3879646e0109fcc944c94430523bf8
Leaderson <leadhason111@gmail.com>
2025-08-14 16:41:28 +0000

    update SellProductsBanner

--- components/sell-products-banner.tsx
+++ components/sell-products-banner.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export default function SellProductsBanner() {
   return (
-    <section className="bg-emerald-900 text-white py-6  mx-6 sm:py-8">
+    <section className="bg-emerald-900 text-white rounded-xl py-6  mx-6 sm:py-8">
       <div className="container mx-auto px-4 flex flex-col sm:flex-row items-center justify-between gap-4">
         <div className="text-center sm:text-left">
           <h2 className="text-lg sm:text-xl font-medium mb-1">Sell Your Products on Tools Pro</h2>
